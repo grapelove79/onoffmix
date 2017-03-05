e87cd6353853ddd3c71a0b2b6f7b514ae6c621bc
grapelove <grapelove79@gmail.com>
2017-03-05 18:05:11 +0900

    index 제이쿼리 버전 변경

--- js/font.js
+++ js/font.js
@@ -1,6 +1,25 @@
-
 (function(global, $) {
   'use strict';
+
+  var indexOf = [].indexOf || function(prop) {
+      for (var i = 0; i < this.length; i++) {
+          if (this[i] === prop) return i;
+      }
+      return -1;
+  };
+  window.getElementsByClassName = function(className,context) {
+      if (context.getElementsByClassName) return context.getElementsByClassName(className);
+      var elems = document.querySelectorAll ? context.querySelectorAll("." + className) : (function() {
+          var all = context.getElementsByTagName("*"),
+              elements = [],
+              i = 0;
+          for (; i < all.length; i++) {
+              if (all[i].className && (" " + all[i].className + " ").indexOf(" " + className + " ") > -1 && indexOf.call(elements,all[i]) === -1) elements.push(all[i]);
+          }
+          return elements;
+      })();
+      return elems;
+  };
 
   var body = document.body;
   if(navigator.appVersion.indexOf('MSIE 8.0') > 0 || navigator.appVersion.indexOf('MSIE 7.0') > 0) // IE8 이하
